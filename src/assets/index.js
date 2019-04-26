d8f841befe8e0e16b6607ce7105d9ae47b5d2b87
nansol <nancysgithub@gmail.com>
2019-04-26 16:31:04 +0200

    Add Sliders, Dalsi produkty, Rekladmni svitky a Profesionalni osvetleni

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,39 +1,51 @@
 
 let sliderImages = document.querySelectorAll('.slide');
-let leftIcon = document.querySelector('left-icon');
-let rightIcon = document.querySelector('right-icon');
+let left = document.getElementById('left');
+let right = document.getElementById('right');
 let current = 0;
 
-function reset(){
-    for (let i = 0; i < sliderImages.length; i++){
-        sliderImages[i].style.display = 'none';
+
+
+window.onload= function (){
+  function reset() {
+      for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
+      }
     }
-}
-
-function startSlide(){
-    reset();
-    sliderImages[0].style.display = 'block';
-}
-
-function slideLeft(){
-    reset();
-    sliderImages[current -1].style.display = 'block';
-    current--;
-}
-
-leftIcon.addEventListener('click', function(){
-    if (current === 0) {
+    
+    function startSlide() {
+      reset();
+      sliderImages[0].style.display = "block";
+    }
+    
+    function slideLeft() {
+      reset();
+      sliderImages[current - 1].style.display = "block";
+      current--;
+    }
+    
+    function slideRight() {
+      reset();
+      sliderImages[current + 1].style.display = "block";
+      current++;
+    }
+    
+   left.addEventListener("click", function() {
+      if (current === 0) {
         current = sliderImages.length;
-    }
-    slideLeft();
-})
- 
+      }
+      slideLeft();
+    }); 
+    
+    right.addEventListener("click", function() {
+      if (current === sliderImages.length - 1) {
+        current = -1;
+      }
+      slideRight();
+    });
+    
+    startSlide();    }
 
-
-startSlide();
-
-
-
-
+  
 
 
